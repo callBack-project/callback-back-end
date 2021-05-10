4fe30874f766e3bad99b6dd33f8a21a57b8db385
Depak Borhara <depakborhara@gmail.com>
2021-05-10 01:31:51 -0400

    Ternary issues fixed

--- api/jobReply.js
+++ api/jobReply.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { JobReply } = require('../db/models');
 
+
 //GET ALL JOB REPLIES
 router.get('/', async (req, res, next) => {
   try {

f6f7d39dc87e142cb08347b58295f42cff1460dc
Alex Paul <al.9thousand@gmail.com>
2021-05-11 17:05:34 -0400

    feat: write companies seed data

--- seed.js
+++ seed.js
@@ -1,5 +1,6 @@
 const db = require('./db');
 const User = require('./db/models/User');
+const Company = require('./db/models/Company');
 
 async function seed() {
     await db.sync({ force: true });
@@ -30,7 +31,30 @@ async function seed() {
           password: "password123"
         }),
       ]);
-    console.log(`seeded ${seedUsers.length} users`);
+      const seedCompanies = await Promise.all([
+        Company.create({
+          name: 'Apple',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus mi est, sollicitudin eget est quis, aliquet congue mi',
+          industry: 'Technology',
+          size: 'Large',
+          location: 'Cupertino, CA',
+        }),
+        Company.create({
+          name: 'Bloomberg',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut pretium neque ac ipsum tincidunt, nec molestie odio iaculis.',
+          industry: 'Software',
+          size: 'Large',
+          location: 'New York, NY'
+        }),,
+        Company.create({
+          name: 'Goldman Sachs',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ac mauris faucibus, consequat ex ut, laoreet diam.',
+          industry: 'Finance',
+          size: 'Large',
+          location: 'New York, NY'
+        }),
+      ]);
+    console.log(`seeded ${seedCompanies.length} Companies`);
     console.log(`seeded successfully`);
   }
   // We've separated the `seed` function from the `runSeed` function.

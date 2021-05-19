a101837dd26c3ff45bdd644d6a3982c6884cd8f5
Alex Paul <al.9thousand@gmail.com>
2021-05-19 16:34:26 -0400

    feat(seed/events.js): create events seed

---
+++ seed/events.js
@@ -0,0 +1,24 @@
+const { Event } = require('../db/models')
+
+const seedEvents = async () => {
+   await Promise.all([
+    Event.create({
+      name: 'Algo Tuesday',
+      location: 'NYC',
+      date: new Date('May 17, 2021 03:00:00'),
+      description: 'algo practice on tuesdays',
+      link: 'meetup.com',
+      likes: 10,
+    }),
+    Event.create({
+      name: 'Tech Pannel',
+      location: 'Fullstack Academy',
+      date: new Date('May 23, 2021 05:00:00'),
+      description: 'Pannel of speakers',
+      link: 'facebook.com',
+      likes: 20,
+    }),
+  ]);
+};
+
+module.exports = seedEvents;
